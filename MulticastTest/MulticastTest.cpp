4a34858542dfd0299b2241459dc5706fa54dc474
Michael Becker <michael.f.becker@gmail.com>
2016-08-10 15:48:57 -0400

    Restructuring to handle multiple interfaces.

--- MulticastTest/MulticastTest.cpp
+++ MulticastTest/MulticastTest.cpp
@@ -20,6 +20,7 @@
 #include <Windows.h>
 #include <Winsock2.h>
 #include <Ws2tcpip.h>
+#include <iphlpapi.h>
 
 #include <errno.h>
 #include <string.h>
@@ -35,34 +36,108 @@
 using namespace std;
 
 
-unsigned short int port;
-unsigned long group_address;
+unsigned short int multicastGroupPort;
+unsigned long multicastGroupAddress;
+DWORD multicastTtl;
 
 
-DWORD WINAPI RecevingThread(_In_ LPVOID lpParameter)
+class MulticastInterface 
+{
+	public:
+		MulticastInterface();
+
+	private:
+		int receiveSocket;
+		int sendSocket;
+		MIB_IPADDRTABLE IPAddrTable;
+
+		static DWORD WINAPI RecevingThread(_In_ LPVOID lpParameter);
+		static DWORD WINAPI SendingThread(_In_ LPVOID lpParameter);
+};
+
+
+MulticastInterface::MulticastInterface()
+{
+	DWORD NoMulticastLoopback = FALSE;
+
+	if ((sendSocket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		cout << "send socket error " << WSAGetLastError() << endl;
+		exit(1);
+	}
+
+	if (setsockopt(sendSocket, IPPROTO_IP, IP_MULTICAST_TTL, (const char *)&multicastTtl, sizeof(multicastTtl)) < 0) {
+		cout << "ttl setsockopt error " << WSAGetLastError() << endl;
+		exit(1);
+	}
+
+	/* Disable Loop-back */
+	if (setsockopt(sendSocket, IPPROTO_IP, IP_MULTICAST_LOOP, (const char *)&NoMulticastLoopback, sizeof(NoMulticastLoopback)) < 0) {
+		cout << "loop setsockopt error " << WSAGetLastError() << endl;
+		exit(1);
+	}
+
+	//	
+	//	We create a new threads, not fork() on Windows.
+	//
+	CreateThread(NULL, 0, RecevingThread, this, 0, NULL);
+	CreateThread(NULL, 0, SendingThread, this, 0, NULL);
+};
+
+
+DWORD WINAPI MulticastInterface::SendingThread(_In_ LPVOID lpParameter)
+{
+	MulticastInterface *mi = (MulticastInterface *)lpParameter;
+
+	struct sockaddr_in mcast_group;
+	memset(&mcast_group, 0, sizeof(mcast_group));
+	mcast_group.sin_family = AF_INET;
+	mcast_group.sin_addr.s_addr = multicastGroupAddress;
+	mcast_group.sin_port = htons(multicastGroupPort);
+
+	for (;;) {
+		string line;
+		size_t len = 0;
+
+		do {
+			getline(cin, line);
+
+		} while (line.length() <= 0);
+
+		int rc = sendto(mi->sendSocket, line.c_str(), line.length(), 0, (struct sockaddr*)&mcast_group, sizeof(mcast_group));
+		if ((rc == SOCKET_ERROR) && rc < (int)line.length()) {
+			cout << "sendto error " << WSAGetLastError() << endl;
+			exit(1);
+		}
+	}
+}
+
+
+DWORD WINAPI MulticastInterface::RecevingThread(_In_ LPVOID lpParameter)
 {
 	int n;
 	int len;
 	struct sockaddr_in from;
 	char message[MAXLEN + 1];
 	struct sockaddr_in mcast_group;
-	int recv_s;
 	struct ip_mreq mreq;
 	BOOL ReuseAddress = TRUE;
 	DWORD NoMulticastLoopback = FALSE;
+	
+	MulticastInterface *mi = (MulticastInterface *)lpParameter;
 
-	if ((recv_s = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+
+	if ((mi->receiveSocket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
 		cout << "recv socket error " << WSAGetLastError() << endl;
 		exit(1);
 	}
 
-	if (setsockopt(recv_s, SOL_SOCKET, SO_REUSEADDR, (const char *)&ReuseAddress, sizeof(ReuseAddress)) < 0) {
+	if (setsockopt(mi->receiveSocket, SOL_SOCKET, SO_REUSEADDR, (const char *)&ReuseAddress, sizeof(ReuseAddress)) < 0) {
 		cout << "reuseaddr setsockopt error " << WSAGetLastError() << endl;
 		exit(1);
 	}
 
 	/* Disable Loop-back */
-	if (setsockopt(recv_s, IPPROTO_IP, IP_MULTICAST_LOOP, (const char *)&NoMulticastLoopback, sizeof(NoMulticastLoopback)) < 0) {
+	if (setsockopt(mi->receiveSocket, IPPROTO_IP, IP_MULTICAST_LOOP, (const char *)&NoMulticastLoopback, sizeof(NoMulticastLoopback)) < 0) {
 		cout << "loop setsockopt error " << WSAGetLastError() << endl;
 		exit(1);
 	}
@@ -70,17 +145,17 @@ DWORD WINAPI RecevingThread(_In_ LPVOID lpParameter)
 	memset(&mcast_group, 0, sizeof(mcast_group));
 	mcast_group.sin_family = AF_INET;
 	mcast_group.sin_addr.s_addr = htonl(INADDR_ANY);
-	mcast_group.sin_port = htons(port);
+	mcast_group.sin_port = htons(multicastGroupPort);
 
 
-	if (bind(recv_s, (struct sockaddr*)&mcast_group, sizeof(mcast_group)) < 0) {
+	if (bind(mi->receiveSocket, (struct sockaddr*)&mcast_group, sizeof(mcast_group)) < 0) {
 		cout << "bind error " << WSAGetLastError() << endl;
 		exit(1);
 	}
 
-	mreq.imr_multiaddr.s_addr = group_address;
+	mreq.imr_multiaddr.s_addr = multicastGroupAddress;
 	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-	if (setsockopt(recv_s, IPPROTO_IP, IP_ADD_MEMBERSHIP, (const char *)&mreq, sizeof(mreq)) == SOCKET_ERROR) {
+	if (setsockopt(mi->receiveSocket, IPPROTO_IP, IP_ADD_MEMBERSHIP, (const char *)&mreq, sizeof(mreq)) == SOCKET_ERROR) {
 		cout << "add_membership setsockopt error " << WSAGetLastError() << endl;
 		exit(1);
 	}
@@ -88,7 +163,7 @@ DWORD WINAPI RecevingThread(_In_ LPVOID lpParameter)
 	for (;;) {
 
 		len = sizeof(from);
-		if ((n = recvfrom(recv_s, message, MAXLEN, 0, (struct sockaddr*)&from, &len)) < 0) {
+		if ((n = recvfrom(mi->receiveSocket, message, MAXLEN, 0, (struct sockaddr*)&from, &len)) < 0) {
 			perror("recv");
 			exit(1);
 		}
@@ -111,14 +186,11 @@ DWORD WINAPI RecevingThread(_In_ LPVOID lpParameter)
 
 int main(int argc, char* argv[])
 {
-	DWORD NoMulticastLoopback = FALSE;
-	int send_s;     
-	DWORD ttl;
 	int iResult;
 	WSADATA wsaData;
 
 	if ((argc<3) || (argc>4)) {
-		fprintf(stderr, "Usage: %s mcast_group port [ttl]\n", argv[0]);
+		fprintf(stderr, "Usage: %s MulticastTest port [ttl]\n", argv[0]);
 		exit(1);
 	}
 
@@ -128,59 +200,22 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-
-	port = (unsigned short int)strtol(argv[2], NULL, 0);
-	group_address = inet_addr(argv[1]);
-
-	if ((send_s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		cout << "send socket error " << WSAGetLastError() << endl;
-		exit(1);
-	}
+	multicastGroupPort = (unsigned short int)strtol(argv[2], NULL, 0);
+	multicastGroupAddress = inet_addr(argv[1]);
 
 	/* If ttl supplied, set it */
 	if (argc == 4) {
-		ttl = strtol(argv[3], NULL, 0);
+		multicastTtl = strtol(argv[3], NULL, 0);
 	}
 	else {
-		ttl = DEFAULT_TTL;
+		multicastTtl = DEFAULT_TTL;
 	}
 
-	if (setsockopt(send_s, IPPROTO_IP, IP_MULTICAST_TTL, (const char *)&ttl, sizeof(ttl)) < 0) {
-		cout << "ttl setsockopt error " << WSAGetLastError() << endl;
-		exit(1);
-	}
 
-	/* Disable Loop-back */
-	if (setsockopt(send_s, IPPROTO_IP, IP_MULTICAST_LOOP, (const char *)&NoMulticastLoopback, sizeof(NoMulticastLoopback)) < 0) {
-		cout << "loop setsockopt error " << WSAGetLastError() << endl;
-		exit(1);
-	}
+	// Find adapters
 
-	//	
-	//	We create a new thread, not fork() on Windows.
-	//
-	CreateThread(NULL, 0, RecevingThread, NULL, 0, NULL);
 
-	struct sockaddr_in mcast_group;
-	memset(&mcast_group, 0, sizeof(mcast_group));
-	mcast_group.sin_family = AF_INET;
-	mcast_group.sin_addr.s_addr = group_address;
-	mcast_group.sin_port = htons(port);
+	// Create objects here
 
-	for (;;) {
-		string line;
-		size_t len = 0;
-
-		do {
-			getline(cin, line);
-
-		} while (line.length() <= 0);
-
-		int rc = sendto(send_s, line.c_str(), line.length(), 0, (struct sockaddr*)&mcast_group, sizeof(mcast_group));
-		if ((rc == SOCKET_ERROR) && rc < (int)line.length()) {
-			cout << "sendto error " << WSAGetLastError() << endl;
-			exit(1);
-		}
-	}
 }
 
